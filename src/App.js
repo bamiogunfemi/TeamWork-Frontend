47e24bb8633d3e24dc2cb5077efdef8ea11b901f
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-12-28 21:46:52 +0100

    created user saga

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React, {useEffect, lazy, Suspense} from "react";
 import SignUp from "./pages/SignUp/signup";
 import SignIn from "./pages/SignIn/signin";
 import Dashboard from "./pages/Dashboard/dashboard";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import {connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect'
+import {selectCurrentUser} from './redux/user/user.selector'
 
-function App() {
+
+const App=({currentUser})=> {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/signin" component={SignIn} />
+        <Route 
+        exact
+         path="/signin"
+        render={()=> currentUser ? 
+        <Redirect to ='/'/>: <SignIn/>} />
         <Route exact path="/signup" component={SignUp} />
         <Route  path="/" component={Dashboard} />
       </Switch>
     </div>
   );
 }
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+})
 
-export default App;
+export default connect(mapStateToProps)(App);
