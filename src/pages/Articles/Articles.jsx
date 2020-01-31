ef745419848cb859ea68c78f0044eb369ea34308
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-01-31 23:55:26 +0100

    fixed new article button

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -4,11 +4,16 @@ import CustomButton from '../../component/custombutton/custombutton'
 import PostContainer from '../../component/post-container/post-container'
 const Articles = () => (
   <div>
-    <div className="center" style ={{position: `fixed`, }}>
-      <CustomButton>Post Article</CustomButton>
+    <div className="center" style={{ position: `fixed`, marginTop: `1rem` }}>
+      <CustomButton>New Article</CustomButton>
+
     </div>
-    <hr />
+    <br />
+    <br />
+    <br />
+
     <PostContainer />
+
   </div>
 )
 
