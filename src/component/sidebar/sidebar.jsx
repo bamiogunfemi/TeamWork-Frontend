274368858dee5fdd80cac55c74d6f329d769e439
Yusuff Faruq <faruqyusuff437@gmail.com>
2020-04-12 22:42:28 +0100

    Improved the styling

--- src/component/sidebar/sidebar.jsx
+++ src/component/sidebar/sidebar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./sidebar.scss";
 import MenuItem from "../menu-item/menu-item";
-import { FaClone,FaCommentAlt,FaSortDownFaTrashAlt,FaListUl,FaUserAlt,} from 'react-icons/fa';
-import { MdGif } from "react-icons/md"
-import { IoIosLogOut } from "react-icons/io"
-import {Link } from 'react-router-dom'
+import {
+  FaClone,
+  FaCommentAlt,
+  FaSortDownFaTrashAlt,
+  FaListUl,
+  FaUserAlt,
+} from "react-icons/fa";
+import { MdGif } from "react-icons/md";
+import { IoIosLogOut } from "react-icons/io";
+import { Link } from "react-router-dom";
 class Sidebar extends React.Component {
   constructor() {
     super();
@@ -12,31 +18,39 @@ class Sidebar extends React.Component {
       user: "bami ogunfemi",
       feeds: "",
       isLoggedin: true,
-      jobDes: "Frontend Developer"
+      jobDes: "Frontend Developer",
     };
   }
-  
+
   render() {
     return (
-      <div className="Sidebar">
-        <div className="user-info mw5 center  ">
-        {/* <FaUserAlt className='user-icon'/> */}
-          <h3 className='  f3 primary-info'>{this.state.user}</h3>
-          <p className="secondary f4">{this.state.jobDes}</p>
+      <div className="sidebar-container">
+        <div className="user-info">
+          {/* <FaUserAlt className='user-icon'/> */}
+          <h3 className="primary-info">{this.state.user}</h3>
+          <p className="secondary-info">{this.state.jobDes}</p>
         </div>
         <div className="menu">
-          <p className="secondary">MENU</p>
-          <Link to='/feeds'>
-          <MenuItem ><FaClone className='menu-icon'/> Feed</MenuItem>
+          <h3 className="secondary">MENU</h3>
+          <Link to="/feeds">
+            <MenuItem>
+              <FaClone className="menu-icon icon" /> Feed
+            </MenuItem>
           </Link>
-          <Link  to='/articles'>
-          <MenuItem><FaListUl className='menu-icon'/>Articles</MenuItem>
+          <Link to="/articles">
+            <MenuItem>
+              <FaListUl className="menu-icon icon" />
+              Articles
+            </MenuItem>
           </Link>
-          <Link to='/gifs'>
-          <MenuItem><MdGif className='menu-icon'/>GIFs</MenuItem>
+          <Link to="/gifs">
+            <MenuItem>
+              <MdGif className="menu-icon icon" />
+              GIFs
+            </MenuItem>
           </Link>
         </div>
-        <IoIosLogOut className='exit-icon'/>
+        <IoIosLogOut className="exit-icon icon" />
       </div>
     );
   }
