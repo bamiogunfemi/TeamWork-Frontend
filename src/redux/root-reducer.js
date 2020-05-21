1e59d695b44b5cafce8c2ff475ac4008c48c45e7
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-05-21 01:08:42 +0100

    new popup

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,16 +1,14 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import {firebaseReducer} from "react-redux-firebase";
-import {firestoreReducer} from "redux-firestore";
+import { firebaseReducer } from "react-redux-firebase";
+import { firestoreReducer } from "redux-firestore";
 const persistConfig = {
   key: "root",
   storage,
- 
 };
 const rootReducer = combineReducers({
-  
   firebase: firebaseReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
 });
 export default persistReducer(persistConfig, rootReducer);
